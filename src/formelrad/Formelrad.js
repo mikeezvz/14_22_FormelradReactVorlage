40d8f65ec59142035962ec57644df0764b1a947a
mikeezvz <mike.zogheib@outlook.com>
2025-05-29 11:24:25 +0200

    A clear handler is required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -11,18 +11,15 @@ export default function Formelrad() {
         p: ""
     })
 
-    const calculate = (event) => {
+    const handleClear = (event) => {
         event.preventDefault();
-        console.log("calculate");
-        if (values.i === "" && values.r === "") {
-            /*calculate i and r */
-            setValues(values => ({...values, i: values.p / values.u}));
-            setValues(values => ({...values, r: values.u * values.u / values.p}));
-        } else if (values.i === "" && values.p === "") {
-            /*calculate i and p */
-            setValues(values => ({...values, i: values.u / values.r}));
-            setValues(values => ({...values, p: values.u * values.u / values.r}));
-        } else if (values.u === "" && values.i === "") {
+        console.log("handleClear");
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("handleSubmit")
+        if (values.u === "" && values.i === "") {
             /*calculate u and i */
             setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
             setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
@@ -34,6 +31,14 @@ export default function Formelrad() {
             /*calculate u and p */
             setValues(values => ({...values, u: values.i * values.r}));
             setValues(values => ({...values, p: values.i * values.i * values.r}));
+        } else if (values.i === "" && values.r === "") {
+            /*calculate i and r */
+            setValues(values => ({...values, i: values.p / values.u}));
+            setValues(values => ({...values, r: values.u * values.u / values.p}));
+        } else if (values.i === "" && values.p === "") {
+            /*calculate i and p */
+            setValues(values => ({...values, i: values.u / values.r}));
+            setValues(values => ({...values, p: values.u * values.u / values.r}));
         } else {
             /*calculate r and p */
             setValues(values => ({...values, r: values.u / values.i}));
@@ -48,12 +53,13 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-                <form onSubmit={calculate}>
+                <form onSubmit={handleSubmit}>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <button style={{margin:10}} onClick={handleClear}>Clear</button>
                 </form>
             </section>
         </>
